b3ea0c038e977ffbb52529726b8337c69fee9003
Hoàng Tuấn Minh <hoangtuanminh1104@gmail.com>
2026-03-17 11:51:50 +0700

    fixed promotionRepo

--- src/main/java/com/example/PixelMageEcomerceProject/repository/PromotionRepository.java
+++ src/main/java/com/example/PixelMageEcomerceProject/repository/PromotionRepository.java
@@ -1,10 +1,17 @@
 package com.example.PixelMageEcomerceProject.repository;
 
-import com.example.PixelMageEcomerceProject.entity.Promotion;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.example.PixelMageEcomerceProject.entity.Promotion;
 
 @Repository
 public interface PromotionRepository extends JpaRepository<Promotion, Integer> {
 
+    List<Promotion> findByDiscountType(String discountType);
+
+    List<Promotion> findByStartDateBeforeAndEndDateAfter(LocalDateTime start, LocalDateTime end);
 }
